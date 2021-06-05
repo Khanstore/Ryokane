b1538462c165566dd7807941da45a232f015ec28
unknown <sagar.jayswal7@gmail.com>
2021-06-05 17:35:49 +0545

    fixed fl name in address

--- fl_auth_signup/static/src/js/signup_tab.js
+++ fl_auth_signup/static/src/js/signup_tab.js
@@ -12,13 +12,14 @@ odoo.define('fl_auth_signup.signup_signin_tab', function (require) {
             $("#name").val($("#f_name").val() + " " +$("#l_name").val());
         });
 
-        $('.div_name').prop('readonly', true);
-        $(".div_f_name").on("input", function(){
-            $(".div_name").val($(".div_f_name").val() + " " +$(".div_l_name").val());
+        $('#div_name').prop('readonly', true);
+        $("#div_f_name").on("input", function(){
+            console.log($("#div_f_name").val())
+            $("#div_name").val($("#div_f_name").val() + " " +$("#div_l_name").val());
         });
         
-        $(".div_l_name").on("input", function(){
-            $(".div_name").val($(".div_f_name").val() + " " +$(".div_f_name").val());
+        $("#div_l_name").on("input", function(){
+            $("#div_name").val($("#div_f_name").val() + " " +$("#div_l_name").val());
         });
         
     
