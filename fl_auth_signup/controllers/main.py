e1b40788fce55d018fea8d1c54bbd083a5e4739c
dsk2370 <sagar.jayswal7@gmail.com>
2021-10-09 07:32:00 +0545

    add ed fields

--- fl_auth_signup/controllers/main.py
+++ fl_auth_signup/controllers/main.py
@@ -12,7 +12,7 @@ from odoo.addons.portal.controllers.portal import CustomerPortal
 
 _logger = logging.getLogger(__name__)
 
-class CustomerPortal(Controller):
+class CustomerPortal(CustomerPortal):
 
     MANDATORY_BILLING_FIELDS = ["f_name","l_name","name", "phone", "email", "street", "city", "country_id"]
     OPTIONAL_BILLING_FIELDS = ["zipcode", "state_id", "vat", "company_name"]
