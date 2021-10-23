9df21f2f7dab217149aaa5828566531659248d96
dsk2370 <sagar.jayswal7@gmail.com>
2021-10-23 11:44:13 +0545

    customer also bought

--- website_also_bought_products/static/src/js/also_bought_products.js
+++ website_also_bought_products/static/src/js/also_bought_products.js
@@ -1,41 +1,41 @@
 
- /* Copyright (c) 2018-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>) */
-/* See LICENSE file for full copyright and licensing details. */
-odoo.define('share_the_love.share_the_love', function (require) {
-"use strict";
-$(document).ready(function()
-{
-    var base = require('web_editor.base');
-    var ajax = require('web.ajax');    
-    $('.oe_website_sale').each(function () {
-        var oe_website_sale = this;
-        $('.ab-item').on('click', 'a.sub-qty', function (ev) {
-            ev.preventDefault();
-            var $input = $(this).parent().find("input");
-            var val = $input.val();
-            val = parseFloat(val) - 1;
-            if (val <= 1)
-            val = 1
-            $input.val(val)
-        });
-        $('.ab-item').on('click', 'a.add-qty', function (ev) {
-            ev.preventDefault();
-            var $input = $(this).parent().find("input");
-            var val = $input.val();
-            val = parseFloat(val) + 1;
-            if (val <= 1)
-            val = 1
-            $input.val(val) 
-        });
-        $('.ab-item .add-to-cart').on('click', function (ev){
-            var product_id = $(this).attr("product-id");
-            var qty = $(this).parent().find("input").val();
-            ajax.jsonRpc("/also/bought/add_to_cart", 'call', {'product_id': product_id,'qty':qty}).then(function(data)
-            {
-                location.pathname="/shop/cart";
-            });
-        });
+//  /* Copyright (c) 2018-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>) */
+// /* See LICENSE file for full copyright and licensing details. */
+// odoo.define('share_the_love.share_the_love', function (require) {
+// "use strict";
+// $(document).ready(function()
+// {
+//     var base = require('web_editor.base');
+//     var ajax = require('web.ajax');    
+//     $('.oe_website_sale').each(function () {
+//         var oe_website_sale = this;
+//         $('.ab-item').on('click', 'a.sub-qty', function (ev) {
+//             ev.preventDefault();
+//             var $input = $(this).parent().find("input");
+//             var val = $input.val();
+//             val = parseFloat(val) - 1;
+//             if (val <= 1)
+//             val = 1
+//             $input.val(val)
+//         });
+//         $('.ab-item').on('click', 'a.add-qty', function (ev) {
+//             ev.preventDefault();
+//             var $input = $(this).parent().find("input");
+//             var val = $input.val();
+//             val = parseFloat(val) + 1;
+//             if (val <= 1)
+//             val = 1
+//             $input.val(val) 
+//         });
+//         $('.ab-item .add-to-cart').on('click', function (ev){
+//             var product_id = $(this).attr("product-id");
+//             var qty = $(this).parent().find("input").val();
+//             ajax.jsonRpc("/also/bought/add_to_cart", 'call', {'product_id': product_id,'qty':qty}).then(function(data)
+//             {
+//                 location.pathname="/shop/cart";
+//             });
+//         });
         
-    });
-});
-});
+//     });
+// });
+// });
--- website_also_bought_products/static/src/js/slider_js.js
+++ website_also_bought_products/static/src/js/slider_js.js
@@ -6,57 +6,67 @@ var sync2 = $("#also-bought-products");
 
       sync2.owlCarousel({
         items : 5,
-        itemsDesktop      : [1199,10],
-        itemsDesktopSmall     : [979,10],
-        itemsTablet       : [768,8],
-        itemsMobile       : [479,8],
-        // pagination:true,
-        // responsiveRefreshRate : 100,
-        loop:true,
-        afterInit : function(el){
-          el.find(".owl-item").eq(0).addClass("synced");
+        margin: 20,
+        nav: true,
+        dots: true,
+        lazyLoad: true,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 3,
+                nav: true
+            },
+            1024: {
+                items: items,
+                nav: true
+            }
         }
       });
 
-      function syncPosition(el){
-        var current = this.currentItem;
-        $("#also-bought-products")
-          .find(".owl-item")
-          .removeClass("synced")
-          .eq(current)
-          .addClass("synced")
-        if($("#also-bought-products").data("owlCarousel") !== undefined){
-          center(current)
-        }
+//       function syncPosition(el){
+//         var current = this.currentItem;
+//         $("#also-bought-products")
+//           .find(".owl-item")
+//           .removeClass("synced")
+//           .eq(current)
+//           .addClass("synced")
+//         if($("#also-bought-products").data("owlCarousel") !== undefined){
+//           center(current)
+//         }
 
-      }
+//       }
 
-      function center(number){
-        var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
+//       function center(number){
+//         var sync2visible = sync2.data("owlCarousel").owl.visibleItems;
 
-        var num = number;
-        var found = false;
-        for(var i in sync2visible){
-          if(num === sync2visible[i]){
-            var found = true;
-          }
-        }
+//         var num = number;
+//         var found = false;
+//         for(var i in sync2visible){
+//           if(num === sync2visible[i]){
+//             var found = true;
+//           }
+//         }
 
-        if(found===false){
-          if(num>sync2visible[sync2visible.length-1]){
-            sync2.trigger("owl.goTo", num - sync2visible.length+2)
-          }else{
-            if(num - 1 === -1){
-              num = 0;
-            }
-            sync2.trigger("owl.goTo", num);
-          }
-        } else if(num === sync2visible[sync2visible.length-1]){
-          sync2.trigger("owl.goTo", sync2visible[1])
-        } else if(num === sync2visible[0]){
-          sync2.trigger("owl.goTo", num-1)
-        }
-      }
+//         if(found===false){
+//           if(num>sync2visible[sync2visible.length-1]){
+//             sync2.trigger("owl.goTo", num - sync2visible.length+2)
+//           }else{
+//             if(num - 1 === -1){
+//               num = 0;
+//             }
+//             sync2.trigger("owl.goTo", num);
+//           }
+//         } else if(num === sync2visible[sync2visible.length-1]){
+//           sync2.trigger("owl.goTo", sync2visible[1])
+//         } else if(num === sync2visible[0]){
+//           sync2.trigger("owl.goTo", num-1)
+//         }
+//       }
 
 
 });
